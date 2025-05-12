aa53faa5b20851c85026bd929e477034c052e975
Ahmet Sahin <73609846+ahmet541@users.noreply.github.com>
2025-05-12 21:47:41 +0300

    Delete app/src/main/java/com/CodeSculpture/app/client directory

--- app/src/main/java/com/CodeSculpture/app/client/LlmApiClientInterface.java
+++
@@ -1,7 +0,0 @@
-package com.CodeSculpture.app.client;
-
-import com.CodeSculpture.app.models.interfaces.ChatRequest;
-
-public interface LlmApiClientInterface {
-    String ask(ChatRequest chatRequest) throws Exception;
-}
